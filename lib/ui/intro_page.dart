94f1b2fe0d001fcc6e7606da58625a27daceb27a
Khairul Anas <mafiakanan@gmail.com>
2020-08-31 21:20:17 +0700

    test commit from android studio

---
+++ lib/ui/intro_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Stack(
+              children: [],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:foodeliver/ui/intro_page.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: double.infinity,
+        color: Colors.amber,
         child: Column(
-          children: [],
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {
+                Get.to(IntroPage());
+              },
+              child: ClipRRect(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  width: 100,
+                  color: Colors.white,
+                  child: Image.asset(
+                    'images/food.png',
+                  ),
+                ),
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+            Text(
+              'FOODHUB',
+              style: GoogleFonts.modak(fontSize: 40, color: Colors.white),
+            ),
+          ],
         ),
       ),
     );
